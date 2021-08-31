5b14e7fdfaa17abb5dbda2b61e77e28c5dfa3345
kristinakazlovskaya <kristinagril@icloud.com>
2021-08-31 23:30:36 +0300

    feat: add titles for links and images

--- script.js
+++ script.js
@@ -3,67 +3,57 @@ $(document).ready(function() {
     $('.header_burger, .header_menu').toggleClass('active');
     $('body').toggleClass('lock');
   });
-});
 
-$(document).ready(function(){ 
   $('a.back').click(function() { 
     parent.history.back(); 
     return false;
-  }); 
-}); 
-
-$('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
-});
-
-$(function() {
-  var basicPrice = 105;
-  $('#prescription').on('change', function () {   
-    var val = $(this).find('option:selected').val();
-    if (val == 1) {
-      basicPrice = 105;
-      $('#cost').html('&#36; ' + basicPrice);
-      $('#lenses option:first').prop('selected', true);}
-    if (val == 2) {
-      $('#cost').html('&#36; ' + (basicPrice + 50));
-      basicPrice = 155;
-      $('#lenses option:first').prop('selected', true);}
   });
-  $('#lenses').on('change', function () {   
-    var val2 = $(this).find('option:selected').val();
-    if (val2 == 1) {
-      basicPrice;
-      $('#cost').html('&#36; ' + basicPrice);}
-    if (val2 == 2) {
-      basicPrice;
-      $('#cost').html('&#36; ' + (basicPrice + 30));}
-    if (val2 == 3) {
-      basicPrice;
-      $('#cost').html('&#36; ' + (basicPrice + 50));}
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
   });
-});
 
-$('#first_faq_section').on('click', function () {
-  $('#first_faq_section + label + .faq_text').slideToggle();
-});
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
 
-$('#second_faq_section').on('click', function () {
-  $('#second_faq_section + label + .faq_text').slideToggle();
-});
+  $(function() {
+    var basicPrice = 105;
+    $('#prescription').on('change', function () {   
+      var val = $(this).find('option:selected').val();
+      if (val == 1) {
+        basicPrice = 105;
+        $('#cost').html('&#36; ' + basicPrice);
+        $('#lenses option:first').prop('selected', true);}
+      if (val == 2) {
+        $('#cost').html('&#36; ' + (basicPrice + 50));
+        basicPrice = 155;
+        $('#lenses option:first').prop('selected', true);}
+    });
+    $('#lenses').on('change', function () {   
+      var val2 = $(this).find('option:selected').val();
+      if (val2 == 1) {
+        basicPrice;
+        $('#cost').html('&#36; ' + basicPrice);}
+      if (val2 == 2) {
+        basicPrice;
+        $('#cost').html('&#36; ' + (basicPrice + 30));}
+      if (val2 == 3) {
+        basicPrice;
+        $('#cost').html('&#36; ' + (basicPrice + 50));}
+    });
+  });
 
-$('#third_faq_section').on('click', function () {
-  $('#third_faq_section + label + .faq_text').slideToggle();
+  $('input:checkbox').on('click', function () {
+    $(this).next().next().slideToggle();
+  });
 });
